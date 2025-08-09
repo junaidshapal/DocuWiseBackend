4ec9b6d43429032311baa3bb8a14989e07aeda43
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-08-09 22:24:22 +0500

    User Profiule DTO

---
+++ DocuWise.DTOs/DTOs/Profile/UserProfileDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuWise.DTOs.DTOs.Profile
+{
+    public class UserProfileDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+    }
+
+}
