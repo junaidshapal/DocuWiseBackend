c3e6f8750ad951039160a7b68b4e61f198abb207
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-08-09 22:22:15 +0500

    Upload Text DTO

---
+++ DocuWise.DTOs/DTOs/TextUploadDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuWise.DTOs.DTOs
+{
+    public class TextUploadDto
+    {
+        public string Text { get; set; }
+        public string Title { get; set; }
+    }
+}
