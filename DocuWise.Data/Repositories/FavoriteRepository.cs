84e82e81f131ce1188620323a9d545d5dd733bc5
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-08-12 15:55:25 +0500

    Favorite Repo

---
+++ DocuWise.Data/Repositories/FavoriteRepository.cs
@@ -0,0 +1,55 @@
+﻿using DocuWise.Data.Data;
+using DocuWise.Data.Models;
+using DocuWise.Data.Repositories.IRepositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuWise.Data.Repositories
+{
+    public class FavoriteRepository : IFavoriteRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FavoriteRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddFavoriteAsync(Favorite favorite)
+        {
+            _context.Favorites.Add(favorite);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveFavoriteAsync(string userId, int documentId)
+        {
+            var favorite = await _context.Favorites
+                .FirstOrDefaultAsync(f => f.UserId == userId && f.DocumentId == documentId);
+
+            if (favorite != null)
+            {
+                _context.Favorites.Remove(favorite);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Document>> GetFavoriteDocumentsAsync(string userId)
+        {
+            return await _context.Favorites
+                .Where(f => f.UserId == userId)
+                .Include(f => f.Document)
+                .Select(f => f.Document)
+                .ToListAsync();
+        }
+
+        public async Task<bool> IsFavoriteAsync(int documentId, string userId)
+        {
+            return await _context.Favorites
+                .AnyAsync(f => f.DocumentId == documentId && f.UserId == userId);
+        }
+    }
+}
