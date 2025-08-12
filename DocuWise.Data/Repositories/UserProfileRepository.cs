b1feacc03d0e0736f11c31d09a1ffc76a2b6e6e2
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-08-12 15:55:53 +0500

    UserProfile Repo

---
+++ DocuWise.Data/Repositories/UserProfileRepository.cs
@@ -0,0 +1,39 @@
+﻿using DocuWise.Data.Models;
+using DocuWise.Data.Repositories.IRepositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuWise.Data.Repositories
+{
+    public class UserProfileRepository : IUserProfileRepository
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserProfileRepository(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<User> GetByIdAsync(string userId)
+        {
+            return await _userManager.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            await _userManager.UpdateAsync(user);
+        }
+
+        public async Task<bool> ChangePasswordAsync(User user, string currentPassword, string newPassword)
+        {
+            var result = await _userManager.ChangePasswordAsync(user, currentPassword, newPassword);
+            return result.Succeeded;
+        }
+    }
+
+}
