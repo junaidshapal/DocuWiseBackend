a2339ba370792875c2d47ab0e74c78a2b916c9c7
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-08-12 15:56:16 +0500

    Favorite Entity

---
+++ DocuWise.Data/Models/Favorite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuWise.Data.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public int DocumentId { get; set; }
+        public Document Document { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
