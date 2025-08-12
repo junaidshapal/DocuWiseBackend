35032af34b58f5bd29aaa28c647dfe2eb5bf6811
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-08-12 15:54:58 +0500

    Interface fpr User repo

---
+++ DocuWise.Data/Repositories/IRepositories/IUserProfileRepository.cs
@@ -0,0 +1,17 @@
+﻿using DocuWise.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuWise.Data.Repositories.IRepositories
+{
+    public interface IUserProfileRepository
+    {
+        Task<User> GetByIdAsync(string userId);
+        Task UpdateAsync(User user);
+        Task<bool> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+    }
+
+}
