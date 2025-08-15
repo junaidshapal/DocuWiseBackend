6746d956d468d5733f208ecd9bc6197d85c79fc0
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-08-15 15:41:46 +0500

    Favorite service

---
+++ DocuWise.Data/Services/FavoriteService.cs
@@ -0,0 +1,47 @@
+﻿using DocuWise.Data.Models;
+using DocuWise.Data.Repositories.IRepositories;
+using DocuWise.Data.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuWise.Data.Services
+{
+    public class FavoriteService : IFavoriteService
+    {
+        private readonly IFavoriteRepository _favoriteRepository;
+
+        public FavoriteService(IFavoriteRepository favoriteRepository)
+        {
+            _favoriteRepository = favoriteRepository;
+        }
+
+        public async Task AddFavoriteAsync(string userId, int documentId)
+        {
+            var favorite = new Favorite
+            {
+                UserId = userId,
+                DocumentId = documentId
+            };
+
+            await _favoriteRepository.AddFavoriteAsync(favorite);
+        }
+
+        public async Task RemoveFavoriteAsync(string userId, int documentId)
+        {
+            await _favoriteRepository.RemoveFavoriteAsync(userId, documentId);
+        }
+
+        public async Task<IEnumerable<Document>> GetFavoriteDocumentsAsync(string userId)
+        {
+            return await _favoriteRepository.GetFavoriteDocumentsAsync(userId);
+        }
+
+        public async Task<bool> IsFavoriteAsync(int documentId, string userId)
+        {
+            return await _favoriteRepository.IsFavoriteAsync(documentId, userId);
+        }
+    }
+}
