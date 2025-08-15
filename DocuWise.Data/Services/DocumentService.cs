65e8dab78bd9910aeaaa84178b0a7ac5cc10ac63
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-08-15 15:42:27 +0500

    document service implementation

--- DocuWise.Data/Services/DocumentService.cs
+++ DocuWise.Data/Services/DocumentService.cs
@@ -6,15 +6,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
+using DocuWise.DTOs.DTOs;
+
 
 namespace DocuWise.Data.Services
 {
     public class DocumentService : IDocumentService
     {
         private readonly IDocumentRepository _documentRepository;
+        private readonly IWebHostEnvironment _env;
 
-        public DocumentService(IDocumentRepository documentRepository)
+
+
+        public DocumentService(IDocumentRepository documentRepository, IWebHostEnvironment env)
         {
+            _env = env;
             _documentRepository = documentRepository;
         }
 
@@ -50,5 +58,38 @@ namespace DocuWise.Data.Services
             await _documentRepository.DeleteDocumentAsync(document);
             await _documentRepository.SaveAsync();
         }
+
+        public async Task<(string summary, string keywords, string category)> AnalyzeTextWithAI(string text)
+        {
+            using var httpClient = new HttpClient
+            {
+                Timeout = TimeSpan.FromMinutes(5)
+            };
+
+            var payload = new { text = text };
+            var jsonContent = new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json");
+
+            //var response = await httpClient.PostAsync("http://localhost:5000/analyze-text", jsonContent);
+            var response = await httpClient.PostAsync("http://localhost:5000/analyze", jsonContent);
+            if (!response.IsSuccessStatusCode)
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("AI server error: " + error);
+                return ("AI text analysis failed", "N/A", "Unknown");
+            }
+
+            var resultJson = await response.Content.ReadAsStringAsync();
+            var aiResult = JsonSerializer.Deserialize<AIResponseDto>(resultJson, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            return (
+                aiResult?.Summary ?? "No summary",
+                string.Join(", ", aiResult?.Keywords ?? new List<string>()),
+                aiResult?.Category ?? "Unknown"
+              );
+        }
+
     }
 }
