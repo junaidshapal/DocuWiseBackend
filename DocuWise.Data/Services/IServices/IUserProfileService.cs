158764038828c91defef8e6713805126ced0168b
junaidshapal <mughaljunaidmughal136@gmail.com>
2025-08-16 15:59:48 +0500

    interface fpr user service

---
+++ DocuWise.Data/Services/IServices/IUserProfileService.cs
@@ -0,0 +1,18 @@
+﻿using DocuWise.DTOs.DTOs.Profile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuWise.Data.Services.IServices
+{
+    public interface IUserProfileService
+    {
+        Task<UserProfileDTO> GetProfileAsync(string userId);
+        Task<bool> UpdateProfileAsync(string userId, UpdateProfileDTO dto);
+        Task<bool> ChangePasswordAsync(string userId, ChangePasswordDTO dto);
+        Task<bool> UpdateProfilePictureAsync(string userId, string imageUrl);
+    }
+
+}
